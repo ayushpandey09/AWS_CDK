617fba38861385927319e36fedf920ad5d13bf93
EC2 Default User <ec2-user@ip-172-31-82-69.ec2.internal>
2024-01-29 09:55:54 +0000

    Initial commit

--- aws_prac/aws_prac_stack.py
+++ aws_prac/aws_prac_stack.py
@@ -1,6 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as _lambda,
+    aws_dynamodb as dynamodb,
+    aws_apigateway as apigateway
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -9,11 +12,22 @@ class AwsPracStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsPracQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        
+        table = dynamodb.TableV2(self, "TaskTableDynamoDb",
+            partition_key=dynamodb.Attribute(name="taskId", type=dynamodb.AttributeType.STRING)
+        )
+    
+        create_task_lambda = _lambda.Function(self, "TaskTable",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="create-task.lambda_handler",
+            code=_lambda.Code.from_asset('lambda-functions')
+        )
+    
+        api = apigateway.RestApi(self,'TaskApi')
+    
+        tasks_resource = api.root.add_resource('tasks')
+        tasks_resource.add_method('POST', apigateway.LambdaIntegration(create_task_lambda))
+        
+        
+    
+       
---
+++ lambda-functions/create-task.py
@@ -0,0 +1,35 @@
+import json
+import os
+import uuid
+import boto3
+
+task_table_name = "AwsPracStack-TaskTableDynamoDbF1E54A09-INM3AHHB142V"
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(task_table_name)
+
+def lambda_handler(event,context):
+    reqest_body = json.loads(event['body'])
+    
+    task_id = str(uuid.uuid4())
+    
+    task_title = reqest_body.get('title','')
+    task_description = reqest_body.get('description','')
+    task_status = reqest_body.get('status','pending')
+    
+    table.put_item(
+        Item={
+            'taskId' : task_id,
+            'title' : task_title,
+            'description' : task_description,
+            'status' : task_status
+        }
+    )
+    
+    response = {
+        'statusCode': 200,
+        'body': json.dumps({'taskId': task_id}),
+        'headers': {'Content-Type': 'application/json'}
+    }
+
+    return response
